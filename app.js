cb96e38b01dca2a4e84a239a8d7225d2d95b65b0
zer0ken <a37592777@gmail.com>
2021-08-28 23:38:04 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+import {
+    Ball
+} from './ball.js'
+
+import {
+    Block
+} from './block.js'
+
+class App {
+    constructor() {
+        this.canvas = document.createElement('canvas')
+        this.ctx = this.canvas.getContext('2d')
+        this.pixelRatio = document.pixelRatio > 1 ? 2 : 1
+
+        document.body.appendChild(this.canvas)
+
+        window.addEventListener('resize', this.resize.bind(this), false)
+        window.addEventListener('scroll', this.scroll.bind(this), false)
+        this.resize()
+
+        this.block = new Block(this.stageWidth / 3, 160, this.stageWidth / 4, this.stageHeight * 3 / 10)
+        this.ball = new Ball(this.stageWidth, this.stageHeight, 40, 15)
+
+        window.requestAnimationFrame(this.animate.bind(this))
+    }
+
+    resize() {
+        this.stageWidth = document.body.clientWidth
+        this.stageHeight = document.body.clientHeight
+
+        this.canvas.width = this.stageWidth * this.pixelRatio
+        this.canvas.height = this.stageHeight * this.pixelRatio
+        this.ctx.scale(this.pixelRatio, this.pixelRatio)
+
+        this.block = new Block(this.stageWidth / 3, 80, this.stageWidth / 4, this.stageHeight * 3 / 10)
+    }
+
+    animate(t) {
+        window.requestAnimationFrame(this.animate.bind(this))
+
+        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight)
+
+        this.block.draw(this.ctx)
+        this.ball.draw(this.ctx, this.stageWidth, this.stageHeight, this.block)
+    }
+
+    scroll(e) {
+        console.log(e.deltaY)
+    }
+}
+
+window.onload = () => {
+    new App()
+}
---
+++ ball.js
@@ -0,0 +1,71 @@
+export class Ball {
+    constructor(stageWidth, stageHeight, radius, speed) {
+        this.radius = radius
+        this.vx = speed
+        this.vy = speed
+
+        const diameter = this.radius * 2
+        this.x = diameter + (Math.random() * stageWidth - diameter)
+        this.y = diameter + (Math.random() * stageHeight - diameter)
+    }
+
+    draw(ctx, stageWidth, stageHeight, block) {
+        this.x += this.vx
+        this.y += this.vy
+
+        this.bounceWindow(stageWidth, stageHeight)
+        this.bounceBlock(block)
+
+        ctx.fillStyle = '#fdd700'
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
+        ctx.fill()
+    }
+
+    bounceWindow(stageWidth, stageHeight) {
+        const minX = this.radius
+        const maxX = stageWidth - this.radius
+        const minY = this.radius
+        const maxY = stageHeight - this.radius
+
+        if (this.x <= minX || this.x >= maxX) {
+            this.vx *= -1
+            this.x += this.vx
+        } else if (this.y <= minY || this.y >= maxY) {
+            this.vy *= -1
+            this.y += this.vy
+        }
+    }
+
+    bounceBlock(block) {
+        const minX = block.x - this.radius
+        const maxX = block.maxX + this.radius
+        const minY = block.y - this.radius
+        const maxY = block.maxY + this.radius
+
+        if (this.x >= minX && this.x <= maxX
+            && this.y >= minY && this.y <= maxY) {
+            const distMinX = Math.abs(minX - this.x)
+            const distMaxX = Math.abs(maxX - this.x)
+            const distMinY = Math.abs(minY - this.y)
+            const distMaxY = Math.abs(maxY - this.y)
+
+            const minDistX = distMinX > distMaxX ? distMinX : distMaxX
+            const minDistY = distMinY > distMaxY ? distMinY : distMaxY
+
+
+            if (minDistX < minDistY) {
+                this.vx *= -1
+                this.x += this.vx
+            } else if (minDistX > minDistY) {
+                this.vy *= -1
+                this.y += this.vy
+            } else {
+                this.vy *= -1
+                this.y += this.vy
+                this.vx *= -1
+                this.x += this.vx
+            }
+        }
+    }
+}
